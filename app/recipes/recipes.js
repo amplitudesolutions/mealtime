484ea4e6a102de8a19822b8d16279ffc359842c8
Martin Fedec <martin@amplitudesolutions.ca>
2015-02-10 21:42:16 -0800

    Recipe Additions

--- app/recipes/recipes.js
+++ app/recipes/recipes.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.recipes', ['ngRoute'])
+angular.module('myApp.recipes', ['ngRoute', 'ngAnimate'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/recipes', {
@@ -14,42 +14,64 @@ angular.module('myApp.recipes', ['ngRoute'])
 	var recipeRef = baseRef.child('recipes');
 	$scope.recipes = $firebase(recipeRef).$asArray();
 	//var itemsRef = baseRef.child('items');
-	$scope.items = $firebase(baseRef.child('items')).$asArray();	
-	
+	$scope.items = $firebase(baseRef.child('items')).$asArray();
+
+	$scope.addNewRecipe = false;
+
 	$scope.newRecipe = '';
 	$scope.newIngredients = {};
+	$scope.newSteps = [];
 	$scope.ingredient = '';
+	$scope.step = '';
 
 	$scope.addRecipe = function() {
 		var newRecipe = $scope.newRecipe;
 		var newIngredients = $scope.newIngredients;
+		var newSteps = $scope.newSteps;
 		if (!newRecipe.name.length) {
 			return;
 		}
 		newRecipe.ingredients = newIngredients;
+		
+		var newStep = $scope.step;
+		if (newStep != '') {
+			newSteps.push({detail: newStep});
+		}
+
+		newRecipe.steps = newSteps;
 
 		var newIngredient = $scope.ingredient;
 		if (newIngredient != '') {
 			newRecipe.ingredients[newIngredient.item.$id] = {name: newIngredient.item.name, quantity: newIngredient.quantity, uom: newIngredient.uom};
 		}
-		
+
 		$scope.recipes.$add(newRecipe);
 
-		$scope.newRecipe = '';
-		$scope.ingredient = '';
-		$scope.newIngredients = '';
+		$scope.closeAdd();
 	};
 
 	$scope.addIngredient = function() {
 		var newIngredient = $scope.ingredient;
 		if (newIngredient != '') {
-			console.log(newIngredient);
 			$scope.newIngredients[newIngredient.item.$id] = {name: newIngredient.item.name, quantity: newIngredient.quantity, uom: newIngredient.uom};
 			$scope.ingredient = '';
 		}
 	};
 
 	$scope.addStep = function() {
+		var newStep = $scope.step;
+		if (newStep != '') {
+			$scope.newSteps[$scope.newSteps.length] = {detail: newStep};
+			$scope.step = '';	
+		}
+	};
 
+	$scope.closeAdd = function() {
+		$scope.newRecipe = '';
+		$scope.ingredient = '';
+		$scope.newIngredients = '';
+		$scope.step = '';
+		$scope.newSteps = '';
+		$scope.addNewRecipe = false;
 	};
 }]);
