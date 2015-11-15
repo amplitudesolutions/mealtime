e9d6e9cd6c89807264dbfa21ff817d44d4435b98
Martin Fedec <martin@amplitudesolutions.ca>
2015-11-14 17:07:17 -0800

    Clean up code

--- app/settings/settings.js
+++ app/settings/settings.js
@@ -17,12 +17,8 @@ angular.module('myApp.settings', ['ui.router', 'ngAnimate', 'ngToast'])
 
 .controller('SettingUnitCtrl', ['$scope','$firebase', 'getDBUrl', 'ngToast', 'user', function($scope, $firebase, getDBUrl, ngToast, user) {
 	var baseRef = new Firebase(getDBUrl.path + '/' + user.get().uid);
-	//var settingsRef = baseRef.child('settings');
-	//var listRef = baseRef.child('lists');
 	var unitsRef = baseRef.child('units');
 
-	//$scope.settings = $firebase(settingsRef).$asObject();
-	//$scope.lists = $firebase(listRef).$asArray();
 	$scope.units = $firebase(unitsRef).$asArray();
 	$scope.newUnit = "";
 
